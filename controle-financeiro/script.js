d7a70a0f3d9adb676fb8a7d5e53699f9729db976
Lucas Felix <lucas.fafx@gmail.com>
2025-02-11 09:32:24 -0300

    feat captura os elementos do HTML no JavaScript

---
+++ controle-financeiro/script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("transaction-form");
+const descriptionInput = document.getElementById("description");
+const valueInput = document.getElementById("value");
+const categoryInput = document.getElementById("category");
+const transactionsList = document.getElementById("transactions-list");
+const balanceDisplay = document.getElementById("balance");
